36ef886b55c6a627284e8e1a1f3eb4c72b2c5507
zhouzhi <zhouzhi@100tal.com>
2023-07-06 20:34:48 +0800

    mysql 操作

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+public class User {
+
+    private Long id;
+    private String name;
+
+    // getters and setters...
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // 其他getter和setter...
+}
